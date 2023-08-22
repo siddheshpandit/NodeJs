a7788f185f98e935b47d2706dea89e72918dae74
Siddhesh Pandit <siddheshpandit12@gmail.com>
2023-08-22 17:25:12 +0530

    Basic of Brower Server Interaction

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NodeJs
+
+<!-- LocalHost -->
--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const http=require('http');
+const fs=require('fs');
 
 const server=http.createServer((req,res)=>{
-    console.log("request has been made from browser to server");
+    // console.log("request has been made from browser to server");
+    // console.log(req);
+    res.setHeader('Content-Type','text/html');
+    let path='./views'
+    switch (req.url) {
+        case '/':
+            path+='/index.html';
+            break;
+        case '/about':
+            path+='/about.html';
+            break;
+        default:
+            path+= '/404.html';
+            break;
+    }
+    fs.readFile(path,(err,data)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            // console.log(data);
+            res.write(data);
+            res.end();
+        }
+    });
 })
 
 
-server.listen(3000,'localhost',()=>{
-    console.log("Server listening on port 3000");
+server.listen(4200,'localhost',()=>{
+    console.log("Server listening on port 4200");
 })
